4cd047eb52c56c922fe46451f8b8bcd9b2eb57c7
oneharry <harryofficialchidi@yahoo.com>
2023-03-28 22:58:12 +0000

    puppet config nginx h

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,8 +2,7 @@
 
 exec { 'nginx configuration':
     provider => shell,
-    command  => 'sudo apt-get update'; 'sudo apt-get install -y nginx';
-	     'echo "Hello World!" > /var/www/html/index.html';
-	     'sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://test.com;\\n\\t}" /etc/nginx/sites-available/default';
-	     'sudo service nginx restart'
+    command  => 'sudo apt-get update; sudo apt-get install -y nginx; echo "Hello World!" > /var/www/html/index.html;
+	     sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://test.com;\\n\\t}" /etc/nginx/sites-available/default; 
+	     sudo service nginx restart'
 }
