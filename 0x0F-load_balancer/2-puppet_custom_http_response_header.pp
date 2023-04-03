0668950568a4e28b6c92db2c81824500f1b6ecf0
oneharry <harryofficialchidi@yahoo.com>
2023-04-03 23:02:11 +0000

    advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,39 +1,22 @@
 # Add cutom header to nginx server
 
-#exec {'add_header':
-#       provider => shell,
-#        command  => "sed -i 's/server_name _;/add_header X-Served-By ${hostname};/' /etc/nginx/sites-available/default"
-#}
-#-> exec {'restart nginx':
-#        provider => shell,
-#        command  => 'sudo service nginx restart'
-#}
-
-class nginx_custom_header {
-    package { 'nginx':
-    ensure => installed,
-    }
-
-    service { 'nginx':
-    ensure => running,
-    enable => true,
-    require => Package['nginx'],
-    }
-
-    file { '/etc/nginx/conf.d/custom_headers.conf':
-        ensure  => file,
-        owner   => 'root',
-	group   => 'root',
-	mode    => '0644',
-        content => template('nginx_custom_header/custom_headers.conf.erb'),
-	notify  => Service['nginx'],
-    }
+exec {'add_header':
+    provider => shell,
+    command  => "sed -i \"s/server_name _;/location / {\n\tadd_header X-Served-By ${hostname};\n}/\" etc/nginx/sites-available/default",
 }
+#-> package {'nginx':
+#    ensure => 'present'
+#}
+#-> file_line {
+#    ensure => present,
+#    path   => 'etc/nginx/sites-available/default',
+#    line   => "\tlocation / {
+#	    add_header X-Served-By ${hostname};
+#    }",
+#    match  => 'server_name _;'
 
-file { '/etc/puppetlabs/code/environments/production/modules/nginx_custom_header/templates/custom_headers.conf.erb':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    content => "server {\n\tadd_header X-Served-By \"$::hostname\";\n}\n",
+#}
+-> exec {'restart nginx':
+        provider => shell,
+        command  => 'sudo service nginx restart'
 }
