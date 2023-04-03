97cdeb2f67ca593d7eeef53e2af70320b65a738b
oneharry <harryofficialchidi@yahoo.com>
2023-04-03 23:03:33 +0000

    advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,20 +2,20 @@
 
 exec {'add_header':
     provider => shell,
-    command  => "sed -i \"s/server_name _;/location / {\n\tadd_header X-Served-By ${hostname};\n}/\" etc/nginx/sites-available/default",
+    command  => 'sudo apt-get update',
 }
-#-> package {'nginx':
-#    ensure => 'present'
-#}
-#-> file_line {
-#    ensure => present,
-#    path   => 'etc/nginx/sites-available/default',
-#    line   => "\tlocation / {
-#	    add_header X-Served-By ${hostname};
-#    }",
-#    match  => 'server_name _;'
+-> package {'nginx':
+    ensure => 'present'
+}
+-> file_line {
+    ensure => present,
+    path   => 'etc/nginx/sites-available/default',
+    line   => "\tlocation / {
+	    add_header X-Served-By ${hostname};
+    }",
+    match  => 'server_name _;'
 
-#}
+}
 -> exec {'restart nginx':
         provider => shell,
         command  => 'sudo service nginx restart'
