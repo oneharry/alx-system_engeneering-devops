58442627ca00b5e1dcfc56c88df6c4aa07f2fc32
oneharry <harryofficialchidi@yahoo.com>
2023-04-03 22:20:48 +0000

    Troubleshoot

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,5 +2,5 @@
 
 exec {'add_header':
         provider => 'shell',
-        command  => 'sed -i "s/server_name _;/add_header X-Served-By $HOSTNAME;/" /etc/nginx/sites-available/default sudo service nginx restart'
+        command  => 'sed -i "s/server_name _;/add_header X-Served-By \"<%= @hostname %>\";/" /etc/nginx/sites-available/default sudo service nginx restart'
 }
