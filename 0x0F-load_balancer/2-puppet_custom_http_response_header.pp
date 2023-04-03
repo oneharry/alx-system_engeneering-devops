0fe5de997640b455f8d7d2ef5a0f45272dcc18c3
oneharry <harryofficialchidi@yahoo.com>
2023-04-03 22:37:46 +0000

    hh

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,10 @@
 # Add cutom header to nginx server
 
 exec {'add_header':
-        provider => 'shell',
-        command  => 'sed -i "s/server_name _;/add_header X-Served-By \"<%= @hostname %>\";/" /etc/nginx/sites-available/default sudo service nginx restart'
+        provider => shell,
+        command  => "sed -i 's/server_name _;/add_header X-Served-By ${hostname};/' /etc/nginx/sites-available/default"
+}
+-> exec {'restart nginx':
+        provider => shell,
+        command  => 'sudo service nginx restart'
 }
