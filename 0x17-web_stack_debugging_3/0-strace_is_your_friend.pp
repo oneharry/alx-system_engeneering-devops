6156ac6f3260bc93f957afd15dbb8968fb7d85e1
oneharry <harryofficialchidi@yahoo.com>
2023-05-10 22:34:28 +0000

    fixyj

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fixing 500 err message
 
 exec {'fx-505':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp/-settings.php',
+	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
 	path    => '/bin';
 }
