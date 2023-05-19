a6f2bb1b533c89c3210d01357dbb3f6b02af2fa5
oneharry <harryofficialchidi@yahoo.com>
2023-05-19 01:19:36 +0000

    advamced

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 #new user can login and open file
 exec {'soft':
-    command  => 'sed -i "holberton soft nofile /s/4/4000/" /etc/security/limits.conf',
+    command  => 'sed -i "holberton soft/s/4/4000/" /etc/security/limits.conf',
     provider => 'shell'
 
 }
 
 exec {'hard':
-    command  => 'sed -i "holberton hard nofile /s/5/5000/" /etc/security/limits.conf',
+    command  => 'sed -i "holberton hard/s/5/5000/" /etc/security/limits.conf',
     provider => 'shell'
 }
