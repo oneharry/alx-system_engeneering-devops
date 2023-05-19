f6b677338c43173f200629616c15a6492724922c
oneharry <harryofficialchidi@yahoo.com>
2023-05-19 00:53:52 +0000

    debugged 4g

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,8 @@ exec {'soft':
     command => 'sed -i "s/15/5000/" /etc/default/nginx',
     path    => '/usr/local/bin/:/bin/',
 
-}
+} ->
+
 exec {'restart-nginx':
     command => 'nginx restart',
     path    => '/etc/init.d/',
